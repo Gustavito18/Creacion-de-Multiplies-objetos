909eef8ec3533f20dbb4df83196ee68c84aa704b
Gustavito18 <lmayorgao@miumg.edu.gt>
2022-03-30 15:04:15 -0600

    Elevacion de la potencia

--- ClaseMath.java
+++ ClaseMath.java
@@ -7,21 +7,24 @@ public class ClaseMath {
  public static Scanner s = new Scanner (System.in); 
     
    public static void main(String[] args) {
-       estructuraContro1();
+       estructuraContro2();
       
     }
     
-   //Raiz Cuadrada
-    private static void estructuraContro1() {
-     double valor = 0.0;
-    
-    System.out.println("Ingrese su valor:");
-    valor = Double.parseDouble(s.nextLine());
-    double raizcuadrada = Math.sqrt(valor);
-    
-       System.out.println("Su raiz es de:" + raizcuadrada);
+
+ //Elevacion a Potencia 
+    private static void estructuraContro2() {
+        double b = 0.0, e = 0.0;
+        
+        System.out.println("Ingrese su base: ");
+        b = s.nextDouble();
+        System.out.println("Ingrese su exponente: ");
+        e = s.nextDouble();
+      
+        double elevacion = Math.pow(b, e);
+        
+        System.out.println("Su respuesta es: " + elevacion);
     }
-    
 
 
 }
